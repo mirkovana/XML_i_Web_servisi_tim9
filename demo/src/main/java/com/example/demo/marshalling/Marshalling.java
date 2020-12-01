d8bb1713d6f4dfa82a43c94c09db21960e8675f4
marijakas <makyns98@gmail.com>
2020-12-01 21:21:23 +0100

    Parseri za zalbuNaOdluku

--- demo/src/main/java/com/example/demo/marshalling/Marshalling.java
+++ demo/src/main/java/com/example/demo/marshalling/Marshalling.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.klaseObavestenja.Obavestenje;
+import com.example.demo.klaseZalbaNaOdluku.ZalbaNaOdluku;
 import com.example.demo.klaseZalbaZbogCutanja.ZalbaCutanje;
 
 
@@ -75,6 +76,39 @@ public class Marshalling {
 			
 			// Umesto System.out-a, moÅ¾e se koristiti FileOutputStream
 			marshaller.marshal(zalbaCutanje, new FileOutputStream("./gen/zalbaZbogCutanja.xml"));
+			
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@GetMapping(path = "/marshalling_zalbaOdluka")
+	public void marshallingZalbaOdluka() {
+		try {
+			System.out.println("[INFO] Example 2: JAXB unmarshalling/marshalling.\n");
+			
+			// DefiniÅ¡e se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
+			JAXBContext context = JAXBContext.newInstance("com.example.demo.klaseZalbaNaOdluku");
+			
+			// Unmarshaller je objekat zaduÅ¾en za konverziju iz XML-a u objektni model
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+
+			ZalbaNaOdluku obavestenje = (ZalbaNaOdluku) unmarshaller.unmarshal(new File("./data/zalbanaodlukucir.xml"));
+			
+			// Izmena nad objektnim modelom dodavanjem novog odseka
+//			fakultet.getOdsek().add(createOdsek("23", "GRID"));
+			
+			// Marshaller je objekat zaduÅ¾en za konverziju iz objektnog u XML model
+			Marshaller marshaller = context.createMarshaller();
+			
+			// PodeÅ¡avanje marshaller-a
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			
+			// Umesto System.out-a, moÅ¾e se koristiti FileOutputStream
+			marshaller.marshal(obavestenje, new FileOutputStream("./gen/zalbaNaOdluku.xml"));
 			
 			
 		} catch (JAXBException e) {
--- demo/src/main/java/com/example/demo/marshalling/Unmarshalling.java
+++ demo/src/main/java/com/example/demo/marshalling/Unmarshalling.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.klaseObavestenja.Obavestenje;
+import com.example.demo.klaseZalbaNaOdluku.ZalbaNaOdluku;
 import com.example.demo.klaseZalbaZbogCutanja.ZalbaCutanje;
 
 
@@ -65,4 +66,29 @@ public class Unmarshalling {
 			e.printStackTrace();
 		}
 	}
+
+	@GetMapping(path = "/unmarshalling_zalbaOdluka")
+	public void unmarshallingzalbaOdluka() {
+		System.out.println();
+		try {
+			
+			System.out.println("[INFO] Example 1: JAXB unmarshalling.\n");
+			
+			// DefiniÅ¡e se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
+			JAXBContext context = JAXBContext.newInstance("com.example.demo.klaseZalbaNaOdluku");
+			
+			// Unmarshaller je objekat zaduÅ¾en za konverziju iz XML-a u objektni model
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			
+			// Unmarshalling generiÅ¡e objektni model na osnovu XML fajla 
+			ZalbaNaOdluku zalbaNaOdluku = (ZalbaNaOdluku) unmarshaller.unmarshal(new File("./data/zalbanaodlukucir.xml"));
+
+			// Prikazuje unmarshallovan objekat
+//			System.out.println(obavestenje.getNaslov());
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
