6c85db9e1e51baf694562be7613e6a5899ac3341
alen1010 <alenmujo10@gmail.com>
2020-12-07 22:35:32 +0100

    a

---
+++ demo/src/main/java/com/example/demo/zahtev/DOMParserZahtev.java
@@ -0,0 +1,250 @@
+package com.example.demo.zahtev;
+
+import java.io.File;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Entity;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.ProcessingInstruction;
+import org.w3c.dom.Text;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import static org.apache.xerces.jaxp.JAXPConstants.*;
+
+/**
+ * 
+ * Kao rezultat parsiranja generiše se objektni reprezent XML dokumenta u vidu
+ * DOM stabla njegovih elemenata. 
+ * 
+ * Primer demonstrira upotrebu metoda API-ja za potrebe pristupanja pojedinim
+ * elementima DOM stabla. 
+ * 
+ * Primer omogućuje validaciju XML fajla u odnosu na XML šemu, koja se svodi 
+ * na postavljanje svojstava factory objekta uz opcionu implementaciju error 
+ * handling metoda.
+ * 
+ * NAPOMENA: za potrebe testiranja validacije dodati nepostojeći element ili 
+ * atribut (npr. "test") u XML fajl koji se parsira.
+ * 
+ */
+public class DOMParserZahtev implements ErrorHandler {
+
+	private static DocumentBuilderFactory factory;
+	
+	private Document document;
+	
+	/*
+	 * Factory initialization static-block
+	 */
+	static {
+		factory = DocumentBuilderFactory.newInstance();
+		
+		/* Uklju�?uje validaciju. */ 
+		factory.setValidating(true);
+		
+		factory.setNamespaceAware(true);
+		factory.setIgnoringComments(true);
+		factory.setIgnoringElementContentWhitespace(true);
+		
+		/* Validacija u odnosu na XML šemu. */
+		factory.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
+	}
+	
+	/**
+	 * Generates document object model for a given XML file.
+	 * 
+	 * @param filePath XML document file path
+	 */
+	public void buildDocument(String filePath) {
+
+		try {
+			
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+			/* Postavlja error handler. */
+			builder.setErrorHandler(this);
+			
+			document = builder.parse(new File(filePath)); 
+
+			/* Detektuju eventualne greske */
+			if (document != null)
+				System.out.println("[INFO] File parsed with no errors.");
+			else
+				System.out.println("[WARN] Document is null.");
+
+		} catch (SAXParseException e) {
+			
+			System.out.println("[ERROR] Parsing error, line: " + e.getLineNumber() + ", uri: " + e.getSystemId());
+			System.out.println("[ERROR] " + e.getMessage() );
+			System.out.print("[ERROR] Embedded exception: ");
+			
+			Exception embeddedException = e;
+			if (e.getException() != null)
+				embeddedException = e.getException();
+
+			// Print stack trace...
+			embeddedException.printStackTrace();
+			
+			System.exit(0);
+			
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Ispis pojedinih elemenata i atributa DOM 
+	 * stabla upotrebom DOM API-ja.
+	 */
+	public void printElement() {
+		
+		System.out.println("Prikaz sadržaja DOM stabla parsiranog XML dokumenta.");
+		//Scanner scanner = new Scanner(System.in);
+	    String elementName, attrName, choice = "";
+	    Element element;
+	    
+	    printNode(document);
+	    
+		System.out.println("[INFO] Kraj.");
+	}
+	
+	/**
+	 * A recursive helper method for iterating 
+	 * over the elements of a DOM tree.
+	 * 
+	 * @param node current node
+	 */
+	private void printNode(Node node) {
+		
+		// Uslov za izlazak iz rekurzije
+		if (node == null)
+			return;
+
+		// Ispis uopštenih podataka o �?voru iz Node interfejsa
+		// printNodeDetails(node, indent);
+		
+		// Ako je upitanju dokument �?vor (korenski element)
+		if (node instanceof Document) {
+			
+			System.out.println("START_DOCUMENT");
+
+			// Rekurzivni poziv za prikaz korenskog elementa
+			Document doc = (Document) node;
+			printNode(doc.getDocumentElement());
+		} else if (node instanceof Element) {
+			
+			Element element = (Element) node;
+			
+			System.out.print("START_ELEMENT: " + element.getTagName());
+
+			// Preuzimanje liste atributa
+			NamedNodeMap attributes = element.getAttributes();
+
+			if (attributes.getLength() > 0) {
+				
+				System.out.print(", ATTRIBUTES: ");
+				
+				for (int i = 0; i < attributes.getLength(); i++) {
+					Node attribute = attributes.item(i);
+					printNode(attribute);
+					if (i < attributes.getLength()-1)
+	        			System.out.print(", ");
+				}
+			}
+			
+			System.out.println();
+			
+			// Prikaz svakog od child nodova, rekurzivnim pozivom
+			NodeList children = element.getChildNodes();
+			
+			if (children != null) {
+				for (int i = 0; i < children.getLength(); i++) {
+					Node aChild = children.item(i);
+					printNode(aChild);
+				}
+			}
+		} 	
+		// Za naredne Cvorove nema rekurzivnog poziva jer ne mogu imati podelemente
+		else if (node instanceof Attr) {
+
+			Attr attr = (Attr) node;
+			System.out.print(attr.getName() + "=" + attr.getValue());
+			
+		}
+		else if (node instanceof Text) {
+			Text text = (Text) node;
+			
+			if (text.getTextContent().trim().length() > 0)
+				System.out.println("CHARACTERS: " + text.getTextContent().trim());
+		}
+		else if (node instanceof CDATASection) {
+			System.out.println("CDATA: " + node.getNodeValue());
+		}
+		else if (node instanceof Comment) {
+			System.out.println("COMMENT: " + node.getNodeValue());
+		}
+		else if (node instanceof ProcessingInstruction) {
+			System.out.print("PROCESSING INSTRUCTION: ");
+
+			ProcessingInstruction instruction = (ProcessingInstruction) node;
+			System.out.print("data: " + instruction.getData());
+			System.out.println(", target: " + instruction.getTarget());
+		}
+		else if (node instanceof Entity) {
+			Entity entity = (Entity) node;
+			System.out.println("ENTITY: " + entity.getNotationName());
+		}
+	}
+	
+	/*
+	 * Error handling methods
+	 */
+
+	@Override
+	public void error(SAXParseException err) throws SAXParseException {
+		// Propagate the exception
+		throw err;
+	}
+
+	@Override
+	public void fatalError(SAXParseException err) throws SAXException {
+		// Propagate the exception
+		throw err;
+	}
+	
+	@Override
+    public void warning(SAXParseException err) throws SAXParseException {
+    	System.out.println("[WARN] Warning, line: " + err.getLineNumber() + ", uri: " + err.getSystemId());
+        System.out.println("[WARN] " + err.getMessage());
+    }
+
+	public static void test() {
+
+		String filePath = null;
+
+		System.out.println("[INFO] DOM Parser");
+		filePath = "./data/zahtev.xml";
+
+		DOMParserZahtev handler = new DOMParserZahtev();
+
+		// Kreiranje DOM stabla na osnovu XML fajla
+		handler.buildDocument(filePath);
+
+		// Prikaz sadržaja korišćenjem DOM API-ja 
+		handler.printElement();
+	}
+}
---
+++ demo/src/main/java/com/example/demo/zahtev/DOMWriterZahtev.java
@@ -0,0 +1,280 @@
+package com.example.demo.zahtev;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+/**
+ * 
+ * Primer demonstrira metode API-ja za potrebe programskog kreiranja DOM stabla. 
+ * Pored programskog kreiranja DOM stabla, primer demonstrira i serijalizaciju
+ * DOM stabla na proizvoljan stream (npr. FileOutputStream, System.out, itd.).
+ *
+ */
+public class DOMWriterZahtev {
+
+	private static String TARGET_NAMESPACE = "http://www.projekat.org/zahtev";
+
+	private static String XSI_NAMESPACE = "http://www.w3.org/2001/XMLSchema-instance";
+	
+	private static DocumentBuilderFactory factory;
+	
+	private static TransformerFactory transformerFactory;
+	
+	private Document document;
+	
+	/*
+	 * Factory initialization static-block
+	 */
+	static {
+		factory = DocumentBuilderFactory.newInstance();
+		
+		transformerFactory = TransformerFactory.newInstance();
+	}
+	
+	/**
+	 * Generates document object model for a given XML file.
+	 */
+	public void createDocument() {
+
+		try {
+			
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+			// Kreiranje novog dokumenta 
+			document = builder.newDocument(); 
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Generates sample document object model 
+	 * programmatically using DOM API methods. 
+	 */
+	public void generateDOM() {
+		
+		// Kreiranje i postavljanje korenskog elementa
+		Element zahtev = document.createElementNS(TARGET_NAMESPACE, "zahtev");	
+		zahtev.setAttributeNS(XSI_NAMESPACE, "xsi:schemaLocation", "http://www.projekat.org/zahtev zahtev.xsd");
+		zahtev.setAttribute("xmlns:za", "http://www.projekat.org/zahtev");
+		
+		zahtev.appendChild(institucija("naziv0", "mesto0"));
+		zahtev.appendChild(naslov("4", "bold", "Zahtev za pristupanje informacijama"));
+		zahtev.appendChild(tekstZahteva());
+		zahtev.appendChild(mestoDatum());
+		zahtev.appendChild(podnosilac());
+		zahtev.appendChild(footer());
+
+		document.appendChild(zahtev);
+	}
+	
+	public Element footer() {
+		/*<reference>*U kućici označiti koja zakonska prava na pristup informacijama želite da ostvarite.</reference>
+        <reference>**U kućici označiti način dostavljanja kopije dokumenata.</reference>
+        <reference>***Kada zahtevate drugi način dostavljanja obavezno upisati koji način dostavljanja zahtevate.</reference>*/
+		Element footer = document.createElementNS(TARGET_NAMESPACE, "footer");		
+		footer.appendChild(element("reference", "*U kućici označiti koja zakonska prava na pristup informacijama želite da ostvarite"));
+		footer.appendChild(element("reference", "**U kućici označiti način dostavljanja kopije dokumenata."));
+		footer.appendChild(element("reference", "***Kada zahtevate drugi način dostavljanja obavezno upisati koji način dostavljanja"));
+		return footer;
+	}
+	
+	public Element podnosilac() {
+		Element podnosilac = document.createElementNS(TARGET_NAMESPACE, "podnosilac");		
+			Element osoba = document.createElementNS(TARGET_NAMESPACE, "osoba");		
+			osoba.appendChild(element("ime", "ime0"));
+			osoba.appendChild(element("prezime", "prezime0"));
+		podnosilac.appendChild(osoba);
+		podnosilac.appendChild(element("adresa", "adresa0"));
+		podnosilac.appendChild(element("drugi_podaci", "drugi_podaci0"));
+		podnosilac.appendChild(element("potpis", "potpis"));
+		return podnosilac;
+	}
+	
+	public Element mestoDatum() {
+		Element mestoDatum = document.createElementNS(TARGET_NAMESPACE, "mesto_datum");		
+		mestoDatum.appendChild(text("U "));
+		mestoDatum.appendChild(element("mesto", "mesto1"));
+		mestoDatum.appendChild(text("dana"));
+		mestoDatum.appendChild(element("datum", "10.10.2010."));
+		mestoDatum.appendChild(text("godine"));
+		return mestoDatum;
+	}
+	
+	public Element naslov(String nivo, String stil, String naslovS) {
+		Element naslov = document.createElementNS(TARGET_NAMESPACE, "naslov");
+		naslov.setAttribute("za:nivo", nivo);
+		naslov.setAttribute("za:stil", stil);
+		naslov.appendChild(text(naslovS));
+		return naslov;
+	}
+	
+	public Element tekstZahteva() {
+		Element tekstZahteva = document.createElementNS(TARGET_NAMESPACE, "tekst_zahteva");
+		tekstZahteva.appendChild(uvod());
+		tekstZahteva.appendChild(lista());
+		tekstZahteva.appendChild(text("Ovaj zahtev se odnosi na sledece informacije:"));
+		tekstZahteva.appendChild(element("informacije", "ahda hahd ua dhajdh ah dlakhdah alsh dka "));
+		return tekstZahteva;
+	}
+	
+	public Element uvod() {
+		Element uvod = document.createElementNS(TARGET_NAMESPACE, "uvod");
+		uvod.appendChild(text("Na osnovu"));
+		uvod.appendChild(zakon("clana 15.", "st. 1.", "",
+				"Zakona o slobodnom pristupu informacijama od javnog značaja",
+				"(„Službeni glasnik RS“, br. 120/04, 54/07, 104/09 i 36/10)"));
+		uvod.appendChild(text("od gore navedenog organa zahtevam:*"));
+		return uvod;
+	}
+	
+	public Element lista() {
+		Element lista = document.createElementNS(TARGET_NAMESPACE, "lista");
+		
+		lista.appendChild(stavka("false", "1", "obaveštenje da li poseduje traženu informaciju"));
+		lista.appendChild(stavka("false", "2", "uvid u dokument koji sadrži traženu informaciju"));
+		lista.appendChild(stavka("false", "3", "kopiju dokumenta koji sadrži traženu informaciju"));
+		
+		Element stavka4 = stavka("false", "4", "dostavljanje kopije dokumenta koji sadrži traženu informaciju:**");
+		
+		lista.appendChild(stavka4);
+		Element lista2 = document.createElementNS(TARGET_NAMESPACE, "lista");
+		lista2.appendChild(stavka("false", "5", "postom"));
+		lista2.appendChild(stavka("false", "6", "elektronskom postom"));
+		lista2.appendChild(stavka("false", "7", "faksom"));
+		Element stavka8 = document.createElementNS(TARGET_NAMESPACE, "stavka");
+		stavka8.setAttribute("za:checked", "false");
+		stavka8.setAttribute("za:id", "8");
+		stavka8.appendChild(text("na drugi nacin:***"));
+			Element dn = document.createElementNS(TARGET_NAMESPACE, "drugi_nacin");
+			dn.appendChild(text("neki drugi nacin za dostavu"));
+		stavka8.appendChild(dn);
+		lista2.appendChild(stavka8);
+		
+		stavka4.appendChild(lista2);
+		
+		return lista;
+	}
+	
+	public Element stavka(String checked, String id, String stavkaS) {
+		Element stavka = document.createElementNS(TARGET_NAMESPACE, "stavka");
+		stavka.setAttribute("za:checked", checked);
+		stavka.setAttribute("za:id", id);
+		stavka.appendChild(text(stavkaS));
+		return stavka;
+	}
+	
+	public Element zakon(String clan, String stav, String tacka, String naziv, String glasnik) {
+		Element zakon = document.createElementNS(TARGET_NAMESPACE, "zakon");
+		if(!clan.contentEquals("")) {
+			zakon.appendChild(element("clan", clan));
+		}
+		if(!stav.contentEquals("")) {
+			zakon.appendChild(element("stav", stav));
+		}
+		if(!tacka.contentEquals("")) {
+			zakon.appendChild(element("tacka", tacka));
+		}
+		if(!naziv.contentEquals("")) {
+			zakon.appendChild(element("naziv", naziv));
+		}
+		if(!glasnik.contentEquals("")) {
+			zakon.appendChild(element("glasnik", glasnik));
+		}
+		return zakon;
+	}
+	
+	public Element institucija(String nazivS, String mestoS) {
+		Element institucija = document.createElementNS(TARGET_NAMESPACE, "institucija");
+		institucija.appendChild(naziv(nazivS));
+		institucija.appendChild(text(" u "));
+		institucija.appendChild(mesto(mestoS));
+		return institucija;
+	}
+	
+	public Element mesto(String mestoS) {
+		Element mesto = document.createElementNS(TARGET_NAMESPACE, "mesto");
+		mesto.appendChild(text(mestoS));
+		return mesto;
+	}
+	public Element naziv(String nazivS) {
+		Element naziv = document.createElementNS(TARGET_NAMESPACE, "naziv");
+		naziv.appendChild(text(nazivS));
+		return naziv;
+	}
+	public Text text(String text) {
+		return document.createTextNode(text);
+	}
+	
+	public Element element(String name, String text) {
+		Element element = document.createElementNS(TARGET_NAMESPACE, name);
+		element.appendChild(text(text));
+		return element;
+	}
+	
+	/**
+	 * Serializes DOM tree to an arbitrary OutputStream.
+	 */
+	public void transform(OutputStream out) {
+		try {
+
+			// Kreiranje instance objekta zaduzenog za serijalizaciju DOM modela
+			Transformer transformer = transformerFactory.newTransformer();
+
+			// Indentacija serijalizovanog izlaza
+			transformer.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "2");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+			// Nad "source" objektom (DOM stablo) vrÅ¡i se transformacija
+			DOMSource source = new DOMSource(document);
+
+			// RezultujuÄ‡i stream (argument metode) 
+			StreamResult result = new StreamResult(out);
+
+			// Poziv metode koja vrÅ¡i opisanu transformaciju
+			transformer.transform(source, result);
+
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void test() {
+
+		System.out.println("[INFO] DOM Parser");
+		DOMWriterZahtev handler = new DOMWriterZahtev();
+		// Kreiranje Document Ä?vora
+		handler.createDocument();
+		// Generisanje DOM stabla
+		handler.generateDOM();
+		// Prikaz sadrÅ¾aja (isprobati sa FileOutputStream-om)
+		handler.transform(System.out);
+		try {
+			handler.transform(new FileOutputStream("./gen/zahtev.xml"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
