9430ebaca622cd758488bedf76c1c689b46dc1d8
mirkovana <mana81098@gmail.com>
2020-12-07 19:00:49 +0100

    Ispravka sheme za zalbu zbog cutanja i ispis

--- demo/src/main/java/com/example/demo/marshalling/Unmarshalling.java
+++ demo/src/main/java/com/example/demo/marshalling/Unmarshalling.java
@@ -11,81 +11,97 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.klaseObavestenja.Obavestenje;
 import com.example.demo.klaseZalbaNaOdluku.ZalbaNaOdluku;
+import com.example.demo.klaseZalbaZbogCutanja.Trazlozi;
 import com.example.demo.klaseZalbaZbogCutanja.ZalbaCutanje;
-
-
 
 @RestController
 public class Unmarshalling {
 
-	
 	@GetMapping(path = "/unmarshalling_obavestenje")
 	public void unmarshallingObavestenje() {
 		System.out.println();
 		try {
-			
+
 			System.out.println("[INFO] Example 1: JAXB unmarshalling.\n");
-			
+
 			// DefiniÅ¡e se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
 			JAXBContext context = JAXBContext.newInstance("com.example.demo.klaseObavestenja");
-			
+
 			// Unmarshaller je objekat zaduÅ¾en za konverziju iz XML-a u objektni model
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			
-			// Unmarshalling generiÅ¡e objektni model na osnovu XML fajla 
+
+			// Unmarshalling generiÅ¡e objektni model na osnovu XML fajla
 			Obavestenje obavestenje = (Obavestenje) unmarshaller.unmarshal(new File("./data/Obavestenje1.xml"));
 
 			// Prikazuje unmarshallovan objekat
 //			System.out.println(obavestenje.getNaslov());
-			
+
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@GetMapping(path = "/unmarshalling_zalbaCutanje")
 	public void unmarshallingZalbaCutanje() {
 		System.out.println();
 		try {
-			
+
 			System.out.println("[INFO] Example 1: JAXB unmarshalling.\n");
-			
+
 			// DefiniÅ¡e se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
 			JAXBContext context = JAXBContext.newInstance("com.example.demo.klaseZalbaZbogCutanja");
-			
+
 			// Unmarshaller je objekat zaduÅ¾en za konverziju iz XML-a u objektni model
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			
-			// Unmarshalling generiÅ¡e objektni model na osnovu XML fajla 
-			ZalbaCutanje zalbaCutanje = (ZalbaCutanje) unmarshaller.unmarshal(new File("./data/zalbazbogcutanjacir.xml"));
+
+			// Unmarshalling generiÅ¡e objektni model na osnovu XML fajla
+			ZalbaCutanje zalbaCutanje = (ZalbaCutanje) unmarshaller
+					.unmarshal(new File("./data/zalbazbogcutanjacir.xml"));
 
 			// Prikazuje unmarshallovan objekat
-//			System.out.println(obavestenje.getNaslov());
-			
+			printZalbaCutanje(zalbaCutanje);
+
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void printZalbaCutanje(ZalbaCutanje zalbaCutanje) {
+
+		// Prikaz naziva fakulteta (getter metoda)
+		System.out.println("- Naslov zalbe: " + zalbaCutanje.getNaslov() + "\n");
+		System.out.println("- Podaci o povereniku: " + "\n");
+		System.out.println("    Naziv poverenika: " + zalbaCutanje.getPodaciOPovereniku().getNazivPoverenika() + "\n");
+		System.out.println("    Adresa poverenika: " + zalbaCutanje.getPodaciOPovereniku().getAdresa().getUlica() +" "
+		+zalbaCutanje.getPodaciOPovereniku().getAdresa().getBroj() + ","+ zalbaCutanje.getPodaciOPovereniku().getAdresa().getGrad()+ "\n");
+		System.out.println("- Podaci o podnosiocu zalbe: " + "\n");
+		System.out.println("    Ime podnosioca: "+zalbaCutanje.getPodaciOPodnosiocuZalbe().getIme()+"\n");
+		System.out.println("    Prezime podnosioca: "+zalbaCutanje.getPodaciOPodnosiocuZalbe().getPrezime()+"\n");
+		System.out.println("    Adresa podnosioca: "+zalbaCutanje.getPodaciOPodnosiocuZalbe().getAdresa().getUlica()+" "+zalbaCutanje.getPodaciOPodnosiocuZalbe().getAdresa().getBroj()+", "+zalbaCutanje.getPodaciOPodnosiocuZalbe().getAdresa().getGrad()+"\n");
+		System.out.println("    Drugi podaci za kontakt podnosioca: "+zalbaCutanje.getPodaciOPodnosiocuZalbe().getDrugiPodaciZaKontakt()+"\n");
+		System.out.println("- Podaci o mestu i datumu podnosenja zalbe: " + zalbaCutanje.getPodaciOMestuIDatumuPodnosenjaZalbe().getMesto()+", "+zalbaCutanje.getPodaciOMestuIDatumuPodnosenjaZalbe().getDatum()+ "\n");
 	}
 
 	@GetMapping(path = "/unmarshalling_zalbaOdluka")
 	public void unmarshallingzalbaOdluka() {
 		System.out.println();
 		try {
-			
+
 			System.out.println("[INFO] Example 1: JAXB unmarshalling.\n");
-			
+
 			// DefiniÅ¡e se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
 			JAXBContext context = JAXBContext.newInstance("com.example.demo.klaseZalbaNaOdluku");
-			
+
 			// Unmarshaller je objekat zaduÅ¾en za konverziju iz XML-a u objektni model
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			
-			// Unmarshalling generiÅ¡e objektni model na osnovu XML fajla 
-			ZalbaNaOdluku zalbaNaOdluku = (ZalbaNaOdluku) unmarshaller.unmarshal(new File("./data/zalbanaodlukucir.xml"));
+
+			// Unmarshalling generiÅ¡e objektni model na osnovu XML fajla
+			ZalbaNaOdluku zalbaNaOdluku = (ZalbaNaOdluku) unmarshaller
+					.unmarshal(new File("./data/zalbanaodlukucir.xml"));
 
 			// Prikazuje unmarshallovan objekat
 //			System.out.println(obavestenje.getNaslov());
-			
+
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
